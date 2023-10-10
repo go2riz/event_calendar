d67733ea85eea700f6c1514e4874028aa114e719
go2riz <go2riz@gmail.com>
2023-10-10 12:58:10 +1100

    Remove deprecated scope and finder

--- lib/event_calendar.rb
+++ lib/event_calendar.rb
@@ -49,11 +49,13 @@ module EventCalendar
     
     # Get the events overlapping the given start and end dates
     def events_for_date_range(start_d, end_d, find_options = {})
-      self.scoped(find_options).find(
-        :all,
-        :conditions => [ "(? <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc ],
-        :order => "#{self.quoted_table_name}.#{self.start_at_field} ASC"
-      )
+      # self.scoped(find_options).find(
+      #   :all,
+      #   :conditions => [ "(? <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc ],
+      #   :order => "#{self.quoted_table_name}.#{self.start_at_field} ASC"
+      # )
+      self.where(find_options).order("#{self.quoted_table_name}.#{self.start_at_field} ASC")
+          .where("(? <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc)
     end
     
     # Create the various strips that show events.
